8a5adfdea01c99a1e034ec4cc6184023172bf487
João Pedro <joaoplay16@gmail.com>
2022-10-09 19:53:36 -0300

    add pixabay api interface

---
+++ app/src/main/java/com/playlab/testingapp/data/remote/PixabayAPI.kt
@@ -0,0 +1,16 @@
+package com.playlab.testingapp.data.remote
+
+import com.playlab.testingapp.BuildConfig
+import com.playlab.testingapp.data.remote.responses.ImageResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PixabayAPI {
+
+    @GET("/api/")
+    suspend fun searchForImage(
+        @Query("q") searchQuery: String,
+        @Query("key") apiKey: String = BuildConfig.API_KEY
+    ): Response<ImageResponse>
+}
