d7f8e60cdbe2de6d2f1275e9225b1536ee508f02
João Pedro <joaoplay16@gmail.com>
2022-10-10 21:55:28 -0300

    add shopping viewmodel

---
+++ app/src/main/java/com/playlab/testingapp/ui/ShoppingViewModel.kt
@@ -0,0 +1,55 @@
+package com.playlab.testingapp.ui
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.playlab.testingapp.data.local.ShoppingItem
+import com.playlab.testingapp.data.remote.responses.ImageResponse
+import com.playlab.testingapp.other.Event
+import com.playlab.testingapp.other.Resource
+import com.playlab.testingapp.repositories.ShoppingRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ShoppingViewModel @Inject constructor(
+    private val repository: ShoppingRepository
+) : ViewModel() {
+
+    val shoppingItems = repository.observeAllShoppingItems()
+
+    val totalPrice = repository.observeTotalPrice()
+
+    private val _images = MutableLiveData<Event<Resource<ImageResponse>>>()
+    val images: LiveData<Event<Resource<ImageResponse>>> = _images
+
+    private val _curImageUrl = MutableLiveData<String>()
+    val curImageUrl: LiveData<String> = _curImageUrl
+
+    private val _insertShoppingItemStatus = MutableLiveData<Event<Resource<ShoppingItem>>>()
+    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> =
+        _insertShoppingItemStatus
+
+    fun setCurImageUrl(url: String) {
+        _curImageUrl.postValue(url)
+    }
+
+    fun deleteShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.deleteShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItemIntoDb(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.insertShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItem(name: String, amountString: String, priceString: String) {
+
+    }
+
+    fun searchForImage(imageQuery: String) {
+
+    }
+}
