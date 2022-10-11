2f1cdcda56922e524fce1d7df269d61b45811061
João Pedro <joaoplay16@gmail.com>
2022-10-10 21:52:28 -0300

    add shopping item fragment

---
+++ app/src/main/java/com/playlab/testingapp/ui/ShoppingItemFragment.kt
@@ -0,0 +1,13 @@
+package com.playlab.testingapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.playlab.testingapp.R
+
+
+class ShoppingItemFragment : Fragment(R.layout.fragment_shopping_item) {
+
+}
