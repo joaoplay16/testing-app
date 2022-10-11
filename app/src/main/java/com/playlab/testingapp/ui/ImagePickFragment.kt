7e60550894ef39af44e9220a9fadd49fe77cb30d
João Pedro <joaoplay16@gmail.com>
2022-10-10 21:53:18 -0300

    add image pick fragment

---
+++ app/src/main/java/com/playlab/testingapp/ui/ImagePickFragment.kt
@@ -0,0 +1,12 @@
+package com.playlab.testingapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.playlab.testingapp.R
+
+class ImagePickFragment : Fragment(R.layout.fragment_image_pick) {
+
+}
