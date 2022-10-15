eea1ea3510e59b91442ac0b001a57df5408914c6
João Pedro <joaoplay16@gmail.com>
2022-10-15 15:07:40 -0300

    test navigation to image pick frament

--- app/src/androidTest/java/com/playlab/testingapp/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/playlab/testingapp/ui/AddShoppingItemFragmentTest.kt
@@ -1,23 +1,34 @@
 package com.playlab.testingapp.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.playlab.testingapp.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import com.playlab.testingapp.R
+
 
 @MediumTest
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
 class AddShoppingItemFragmentTest {
+
+    @get:Rule(order = 1)
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
@@ -25,6 +36,21 @@ class AddShoppingItemFragmentTest {
     @Before
     fun setup() {
         hiltRule.inject()
+    }
+
+    @Test
+    fun clickShoppingItemImageView_navigateToImagePickFragment(){
+        val navController = mock(NavController::class.java)
+
+        launchFragmentInHiltContainer<AddShoppingItemFragment> {
+            Navigation.setViewNavController(requireView(), navController)
+        }
+
+        onView(withId(R.id.ivShoppingImage)).perform(click())
+
+        verify(navController).navigate(
+            AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+        )
     }
 
     @Test
