dfe8e730885dfab9388182a6e7cfbc9dcd7f3843
João Pedro <joaoplay16@gmail.com>
2022-10-14 22:55:14 -0300

    setup navigation

--- app/src/main/java/com/playlab/testingapp/ui/AddShoppingItemFragment.kt
+++ app/src/main/java/com/playlab/testingapp/ui/AddShoppingItemFragment.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.playlab.testingapp.R
+import com.playlab.testingapp.databinding.FragmentAddShoppingItemBinding
+import com.playlab.testingapp.databinding.FragmentShoppingBinding
 
 
 class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
 
     lateinit var viewModel: ShoppingViewModel
+    lateinit var binding: FragmentAddShoppingItemBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAddShoppingItemBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+        binding.ivShoppingImage.setOnClickListener{
+            findNavController().navigate(
+                AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+            )
+        }
+
+        val callback = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                viewModel.setCurImageUrl("")
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
     }
 }
--- app/src/main/java/com/playlab/testingapp/ui/ShoppingFragment.kt
+++ app/src/main/java/com/playlab/testingapp/ui/ShoppingFragment.kt
@@ -1,19 +1,41 @@
 package com.playlab.testingapp.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
+import androidx.navigation.fragment.findNavController
 import com.playlab.testingapp.R
+import com.playlab.testingapp.databinding.FragmentShoppingBinding
 
 
 class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
 
     lateinit var viewModel: ShoppingViewModel
+    lateinit var binding: FragmentShoppingBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentShoppingBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+        binding.fabAddShoppingItem.setOnClickListener {
+            findNavController().navigate(
+                ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
+            )
+        }
     }
+
 }
