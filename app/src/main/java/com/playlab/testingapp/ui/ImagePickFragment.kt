907dcb2ab842a13db481c5304ee36aeafc2c32c4
João Pedro <joaoplay16@gmail.com>
2022-10-15 19:08:10 -0300

    override onViewCreated and add a method to setup RecyclerView

--- app/src/main/java/com/playlab/testingapp/ui/ImagePickFragment.kt
+++ app/src/main/java/com/playlab/testingapp/ui/ImagePickFragment.kt
@@ -6,14 +6,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.playlab.testingapp.R
+import com.playlab.testingapp.adapter.ImageAdapter
+import com.playlab.testingapp.databinding.FragmentImagePickBinding
+import com.playlab.testingapp.other.Constants.GRID_SPAN_COUNT
+import javax.inject.Inject
 
-class ImagePickFragment : Fragment(R.layout.fragment_image_pick) {
+class ImagePickFragment @Inject constructor(
+    val imageAdapter: ImageAdapter
+) : Fragment(R.layout.fragment_image_pick) {
 
     lateinit var viewModel: ShoppingViewModel
+
+    lateinit var binding: FragmentImagePickBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentImagePickBinding.inflate(layoutInflater)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        setupRecyclerView()
+
+        imageAdapter.setOnItemClickListener {
+            findNavController().popBackStack()
+            viewModel.setCurImageUrl(it)
+        }
+    }
+
+    private fun setupRecyclerView(){
+        binding.rvImages.apply {
+            adapter = imageAdapter
+            layoutManager = GridLayoutManager(requireContext(), GRID_SPAN_COUNT)
+        }
     }
 }
