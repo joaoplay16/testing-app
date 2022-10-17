54e9bbf69d12c3b87ea67224f802002d7cbb7ef8
João Pedro <joaoplay16@gmail.com>
2022-10-17 15:18:28 -0300

    setup recycler view

--- app/src/main/java/com/playlab/testingapp/ui/ShoppingFragment.kt
+++ app/src/main/java/com/playlab/testingapp/ui/ShoppingFragment.kt
@@ -7,11 +7,22 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.LEFT
+import androidx.recyclerview.widget.ItemTouchHelper.RIGHT
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.playlab.testingapp.R
+import com.playlab.testingapp.adapter.ShoppingItemAdapter
 import com.playlab.testingapp.databinding.FragmentShoppingBinding
+import com.playlab.testingapp.other.Constants
+import javax.inject.Inject
 
 
-class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+class ShoppingFragment @Inject constructor(
+    val shoppingItemAdapter: ShoppingItemAdapter
+): Fragment(R.layout.fragment_shopping) {
 
     lateinit var viewModel: ShoppingViewModel
     lateinit var binding: FragmentShoppingBinding
@@ -38,4 +49,33 @@ class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
         }
     }
 
+    private val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+        0, LEFT or RIGHT
+    ){
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ) = true
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val pos = viewHolder.layoutPosition
+            val item = shoppingItemAdapter.shoppingItems[pos]
+            viewModel.deleteShoppingItem(item)
+            Snackbar.make(requireView(), "Successfully deleted item", Snackbar.LENGTH_LONG).apply {
+                setAction("Undo") {
+                    viewModel.insertShoppingItemIntoDb(item)
+                }
+                show()
+            }
+        }
+    }
+
+    fun setupRecyclerView(){
+        binding.rvShoppingItems.apply {
+            adapter = shoppingItemAdapter
+            layoutManager = GridLayoutManager(requireActivity(), Constants.GRID_SPAN_COUNT)
+            ItemTouchHelper(itemTouchCallback).attachToRecyclerView(this)
+        }
+    }
 }
