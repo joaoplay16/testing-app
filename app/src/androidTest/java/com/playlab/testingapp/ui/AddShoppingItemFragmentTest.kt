14e2e72d985e9040d15eb9dbe51210a2bfc1041a
João Pedro <joaoplay16@gmail.com>
2022-10-16 23:04:11 -0300

    test item insertion

--- app/src/androidTest/java/com/playlab/testingapp/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/playlab/testingapp/ui/AddShoppingItemFragmentTest.kt
@@ -6,10 +6,12 @@ import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.google.common.truth.Truth.assertThat
 import com.playlab.testingapp.R
+import com.playlab.testingapp.data.local.ShoppingItem
 import com.playlab.testingapp.launchFragmentInHiltContainer
 import com.playlab.testingapp.repositories.FakeShoppingRepositoryAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -21,6 +23,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 
 @MediumTest
@@ -34,9 +37,32 @@ class AddShoppingItemFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     @Before
     fun setup() {
         hiltRule.inject()
+    }
+
+    @Test
+    fun clickInsertIntoDd_shoppingItemInsertedIntoDb(){
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("Shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("2"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("5.5"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("Shopping item", 2, 5.5f, "")
+        )
     }
 
     @Test
