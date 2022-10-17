60b2756efd3c6482b28013b0865d0fdc20d958b8
João Pedro <joaoplay16@gmail.com>
2022-10-16 23:01:12 -0300

    setup observers

--- app/src/main/java/com/playlab/testingapp/ui/AddShoppingItemFragment.kt
+++ app/src/main/java/com/playlab/testingapp/ui/AddShoppingItemFragment.kt
@@ -6,14 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import com.playlab.testingapp.R
 import com.playlab.testingapp.databinding.FragmentAddShoppingItemBinding
 import com.playlab.testingapp.databinding.FragmentShoppingBinding
+import com.playlab.testingapp.other.Status
+import javax.inject.Inject
 
 
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+): Fragment(R.layout.fragment_add_shopping_item) {
 
     lateinit var viewModel: ShoppingViewModel
     lateinit var binding: FragmentAddShoppingItemBinding
@@ -31,6 +38,8 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        subscribeToObservers()
+
 
         binding.ivShoppingImage.setOnClickListener{
             findNavController().navigate(
@@ -46,5 +55,35 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.curImageUrl.observe(viewLifecycleOwner, Observer {
+            glide.load(it).into(binding.ivShoppingImage)
+        })
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { result ->
+                when(result.status){
+                    Status.SUCCESS -> {
+                        Snackbar.make(
+                            binding.root,
+                            "Added Shopping Item",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            binding.root,
+                            result.message ?: "An unknown error occurred",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    Status.LOADING -> {
+                        /*NO-OP*/
+                    }
+                }
+            }
+        })
     }
 }
