c3bff13c92848db8271c3847274af437d305c0bf
João Pedro <joaoplay16@gmail.com>
2022-10-19 18:14:10 -0300

    setup observers

--- app/src/main/java/com/playlab/testingapp/ui/ImagePickFragment.kt
+++ app/src/main/java/com/playlab/testingapp/ui/ImagePickFragment.kt
@@ -1,10 +1,11 @@
 package com.playlab.testingapp.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
@@ -12,12 +13,13 @@ import com.playlab.testingapp.R
 import com.playlab.testingapp.adapter.ImageAdapter
 import com.playlab.testingapp.databinding.FragmentImagePickBinding
 import com.playlab.testingapp.other.Constants.GRID_SPAN_COUNT
+import com.playlab.testingapp.other.Status
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class ImagePickFragment @Inject constructor(
-    val imageAdapter: ImageAdapter
+    val imageAdapter: ImageAdapter,
 ) : Fragment(R.layout.fragment_image_pick) {
 
     lateinit var viewModel: ShoppingViewModel
@@ -36,12 +38,28 @@ class ImagePickFragment @Inject constructor(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        setupObservers()
         setupRecyclerView()
 
         imageAdapter.setOnItemClickListener {
             findNavController().popBackStack()
             viewModel.setCurImageUrl(it)
         }
+    }
+
+    private fun setupObservers(){
+        viewModel.images.observe(viewLifecycleOwner, Observer {
+            val imageResource = it.getContentIfNotHandled()
+
+            if(imageResource?.status == Status.SUCCESS){
+                val imageResponse = imageResource.data
+                val images = imageResponse?.hits?.map { imageResult ->
+                    imageResult.largeImageURL
+                }
+
+                imageAdapter.images = images!!
+            }
+        })
     }
 
     private fun setupRecyclerView(){
