2ec783c176b54c05e1d96189be62bd1a6f61d0f3
João Pedro <joaoplay16@gmail.com>
2022-10-19 22:24:56 -0300

    setup timber

--- app/src/main/java/com/playlab/testingapp/ShoppingApplication.kt
+++ app/src/main/java/com/playlab/testingapp/ShoppingApplication.kt
@@ -2,6 +2,13 @@ package com.playlab.testingapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 @HiltAndroidApp
-class ShoppingApplication : Application()
+class ShoppingApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(DebugTree())
+    }
+}
